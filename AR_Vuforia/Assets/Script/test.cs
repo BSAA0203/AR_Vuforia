fa138e33286a991a5114adc19371f6e4c356501d
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-11-23 10:41:42 +0900

    Test rotate obj about touch event

---
+++ AR_Vuforia/Assets/Script/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    Vector3 xyz = new Vector3(15,15,15);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.Rotate(xyz*Time.deltaTime);
+        //this.transform.Translate(xyz*Time.deltaTime);
+        //this.transform.localScale+=xyz;
+    }
+}
---
+++ AR_Vuforia/Assets/rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    float rotate_speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            float rotx= Input.GetAxis("Mouse X")*rotate_speed*Mathf.Deg2Rad;
+            float roty= Input.GetAxis("Mouse Y")*rotate_speed*Mathf.Deg2Rad;
+
+            this.transform.RotateAround(Vector3.up,-rotx);
+            this.transform.RotateAround(Vector3.right,roty);
+        }
+    }
+}
